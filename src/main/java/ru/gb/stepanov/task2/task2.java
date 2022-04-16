54e661c99a1d2dc89d475f81812325b76c0bec29
StepPasha <pphudc@inbox.ru>
2022-04-16 18:53:56 +0300

    Task 2

--- src/main/java/ru/gb/stepanov/task2/task2.java
+++ src/main/java/ru/gb/stepanov/task2/task2.java
@@ -2,11 +2,15 @@ package ru.gb.stepanov.task2;
 
 public class task2 {
     public static void main(String[] args) {
+        methodNumbersPositiveOrNegative();
 
     }
-    public static void methodNumbers(int a) {
+    public static void methodNumbersPositiveOrNegative() {
+        int a = -8;
         if (a >= 0) {
             System.out.println("Positive");
+        }else {
+            System.out.println("negative");
         }
     }
 }
