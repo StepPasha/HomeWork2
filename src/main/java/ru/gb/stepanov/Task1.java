7460dd6d5e6ea52849a74aca3373beb7863424d1
StepPasha <pphudc@inbox.ru>
2022-04-16 18:21:02 +0300

    Task 1

---
+++ src/main/java/ru/gb/stepanov/Task1.java
@@ -0,0 +1,21 @@
+package ru.gb.stepanov;
+
+public class Task1 {
+    public static void main(String[] args) {
+        boolean result;
+        result = method1020(4, 10);
+
+    }
+
+    public static boolean method1020(int a, int b) {
+        int sum = a + b;
+        if (sum > 10 && sum < 20) {
+            System.out.println(true);
+            return true;
+        } else {
+            System.out.println(false);
+            return false;
+        }
+    }
+}
+
